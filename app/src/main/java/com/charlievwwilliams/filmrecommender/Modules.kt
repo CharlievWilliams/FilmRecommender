0980ef60a0b0b9f69c7d99eade2ce9ec3c9c5590
Charlie Williams <charliewilliamsweb@gmail.com>
2021-04-01 11:21:44 +0100

    Remove Database Functionality

--- app/src/main/java/com/charlievwwilliams/filmrecommender/Modules.kt
+++ app/src/main/java/com/charlievwwilliams/filmrecommender/Modules.kt
@@ -1,8 +1,8 @@
 package com.charlievwwilliams.filmrecommender
 
-import com.charlievwwilliams.filmrecommender.viewmodels.SearchViewModel
 import com.charlievwwilliams.filmrecommender.viewmodels.RecommendationsViewModel
 import com.charlievwwilliams.filmrecommender.viewmodels.SearchResultViewModel
+import com.charlievwwilliams.filmrecommender.viewmodels.SearchViewModel
 import com.charlievwwilliams.filmrecommender.viewmodels.SplashViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
